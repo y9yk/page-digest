111519f6503fc988dd63113e99588bfdff77a8d8
y9yk <andrew.yk82@gmail.com>
2024-08-23 13:10:55 +0900

    feat: add logging middleware

--- src/app/main.py
+++ src/app/main.py
@@ -9,7 +9,9 @@ from src.app.errors import (
     global_exception_handler,
     validation_exception_handler,
 )
+from src.app.middlewares.logging_middleware import HttpRequestLoggingMiddleware
 from src.app.modules.api import api_router
+from src.app.utils import get_root_logger
 
 
 # create application
@@ -28,6 +30,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(HttpRequestLoggingMiddleware, logger=get_root_logger())
 
 # register exception handler
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
