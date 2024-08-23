8780a2cf5dd10d7084602209df0211400f850790
y9yk <andrew.yk82@gmail.com>
2024-08-23 13:10:37 +0900

    feat: update router and service

--- src/app/modules/digest/router.py
+++ src/app/modules/digest/router.py
@@ -1,12 +1,10 @@
 import logging
 from fastapi import APIRouter, Request
+from starlette.responses import StreamingResponse
 
-from src.app.common.base_schema import BaseResponse
 from src.app.common.config import settings
-from src.app.common.constant import QueryType
+from src.app.common.base_schema import BaseResponse
 from src.app.modules.digest import DigestContentRequest, DigestContentService
-
-from src.app.errors import NotImplementedException
 
 
 logger = logging.getLogger(settings.PROJECT_NAME)
@@ -19,7 +17,7 @@ router = APIRouter(
 async def get_digest_content(
     request: Request,
     digest_content_request: DigestContentRequest,
-) -> BaseResponse:
+):
     """
     get_digest_content
     """
@@ -27,5 +25,10 @@ async def get_digest_content(
     logger.debug(locals())
 
     # process
-    results = await DigestContentService().get_content()
-    return BaseResponse(results=results)
+    results = await DigestContentService().get_content(
+        **digest_content_request.model_dump(),
+    )
+    return StreamingResponse(
+        results,
+        media_type="text/event-stream",
+    )
--- src/app/modules/digest/service.py
+++ src/app/modules/digest/service.py
@@ -1,11 +1,30 @@
 import logging
 
 from src.app.common.config import settings
+from src.app.clients.screencapture import ScreenCaptureProcessor, get_screencapture_processor
+from src.app.clients.llm import LLM, PageDigestor
 
 
 class DigestContentService(object):
     def __init__(self):
         self.logger = logging.getLogger(settings.PROJECT_NAME)
 
-    async def get_content(self):
-        return {}
+    async def get_content(
+        self,
+        url: str,
+        model: str,
+        max_tokens: int = 2000,
+    ):
+        # clients
+        screen_capture_processor: ScreenCaptureProcessor = get_screencapture_processor()
+        page_digestor: PageDigestor = PageDigestor(
+            client=LLM(
+                model=model,
+                api_key=settings.OPENAI_API_KEY,
+                max_tokens=max_tokens,
+            )
+        )
+
+        # processing and return
+        base64_encoded_image_str = await screen_capture_processor.process(url=url)
+        return await page_digestor.process(page_content=base64_encoded_image_str)
