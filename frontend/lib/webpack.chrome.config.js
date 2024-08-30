40fbf1f8376515cb1c70cbba0dc66cde04c37dc8
y9yk <andrew.yk82@gmail.com>
2024-08-30 16:28:30 +0900

    fix: lint

--- frontend/lib/webpack.chrome.config.js
+++ frontend/lib/webpack.chrome.config.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    content: path.resolve(__dirname, './src/content/index.js'),
-    background: path.resolve(__dirname, './src/background/index.js'),
+    content: path.resolve(__dirname, "./src/content/index.js"),
+    background: path.resolve(__dirname, "./src/background/index.js"),
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, '..', 'chrome', 'build'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "..", "chrome", "build"),
   },
   module: {
     rules: [
@@ -22,31 +22,31 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
           },
         ],
       },
       {
         test: /\.svg$/,
-        type: 'asset/inline',
+        type: "asset/inline",
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: 'lib/chrome/public/manifest.json', to: 'manifest.json' },
-        { from: 'lib/chrome/public/options.js', to: 'options.js' },
-        { from: 'assets/options.html', to: 'options.html' },
-        { from: 'assets/output.css', to: 'styles.css' },
-        { from: 'assets/res/logo-16.png', to: 'logo-16.png' },
-        { from: 'assets/res/logo-32.png', to: 'logo-32.png' },
-        { from: 'assets/res/logo-48.png', to: 'logo-48.png' },
-        { from: 'assets/res/logo-128.png', to: 'logo-128.png' },
+        { from: "lib/chrome/public/manifest.json", to: "manifest.json" },
+        { from: "lib/chrome/public/options.js", to: "options.js" },
+        { from: "assets/options.html", to: "options.html" },
+        { from: "assets/output.css", to: "styles.css" },
+        { from: "assets/res/logo-16.png", to: "logo-16.png" },
+        { from: "assets/res/logo-32.png", to: "logo-32.png" },
+        { from: "assets/res/logo-48.png", to: "logo-48.png" },
+        { from: "assets/res/logo-128.png", to: "logo-128.png" },
       ],
     }),
     new CleanWebpackPlugin(),
