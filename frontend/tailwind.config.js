3d3c3a66922919dae30792a04b390689064a554f
y9yk <andrew.yk82@gmail.com>
2024-08-30 11:49:39 +0900

    feat: add extra config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./lib/src/**/*.js'],
+  prefix: 'sumz-',
+  theme: {
+    extend: {
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
