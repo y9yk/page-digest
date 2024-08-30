9c184fa83144088724bfe74ebf00906f22669b31
y9yk <andrew.yk82@gmail.com>
2024-08-30 12:18:23 +0900

    fix: bug

--- backend/src/app/clients/llm/digestor.py
+++ backend/src/app/clients/llm/digestor.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 
 from src.app.common.config import settings
 from src.app.clients.llm import LLM
@@ -28,28 +29,26 @@ class PageDigestor(object):
                 prompt = "이미지를 텍스트로 변환하고, 요약해서 Markdown 형태로 표현해줘"
                 messages.append(
                     {
-                        {
-                            "role": "user",
-                            "content": [
-                                {"type": "text", "text": f"{prompt}"},
-                                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{content}"}},
-                            ],
-                        },
-                    }
+                        "role": "user",
+                        "content": [
+                            {"type": "text", "text": f"{prompt}"},
+                            {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{content}"}},
+                        ],
+                    },
                 )
             else:
                 prompt = f"content: {content}\n\n위의 content를 해석하고 요약해서 Markdown 형태로 표현해줘"
                 messages.append(
                     {
-                        {
-                            "role": "user",
-                            "content": [
-                                {"type": "text", "text": f"{prompt}"},
-                            ],
-                        },
-                    }
+                        "role": "user",
+                        "content": [
+                            {"type": "text", "text": f"{prompt}"},
+                        ],
+                    },
                 )
 
             return self.client.get_chat_response(messages=messages)
         except Exception as e:
+            if settings.DEBUG:
+                traceback.print_exc()
             self.logger.error(f"Error in page-digest: {e}")
--- backend/src/tests/service/test_page_digest.py
+++ backend/src/tests/service/test_page_digest.py
@@ -16,8 +16,10 @@ async def test_page_digest(
     data_grid_query
     """
     data = {
-        "url": "https://medium.com/dlift/%EC%9E%90%EB%8F%99%EC%9C%BC%EB%A1%9C-%EC%B5%9C%EC%8B%A0-%EA%B8%B0%EC%88%A0-%EB%8F%99%ED%96%A5%EC%9D%84-%EC%A0%95%EB%A6%AC%ED%95%B4%EC%A3%BC%EB%8A%94-%EB%AF%B8%EB%94%94%EC%97%84-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EB%A7%8C%EB%93%A4%EA%B8%B0-5a2585c2ded2",
+        "content": "https://medium.com/dlift/%EC%9E%90%EB%8F%99%EC%9C%BC%EB%A1%9C-%EC%B5%9C%EC%8B%A0-%EA%B8%B0%EC%88%A0-%EB%8F%99%ED%96%A5%EC%9D%84-%EC%A0%95%EB%A6%AC%ED%95%B4%EC%A3%BC%EB%8A%94-%EB%AF%B8%EB%94%94%EC%97%84-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EB%A7%8C%EB%93%A4%EA%B8%B0-5a2585c2ded2",
+        "content_type": "url",
         "model": "gpt-4o",
+        "max_tokens": 2000,
     }
 
     url = pytestconfig.getini("path_page_digest")
