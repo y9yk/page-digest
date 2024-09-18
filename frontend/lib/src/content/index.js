569421c9f3972a9e3f1bc64787e86e0a6ddfc9e2
y9yk <andrew.yk82@gmail.com>
2024-09-18 20:55:26 +0900

    fix: use markdown-renderer and tailwindcss/typography

--- frontend/lib/src/content/index.js
+++ frontend/lib/src/content/index.js
@@ -1,14 +1,29 @@
 import { getContentOfArticle } from "./utils/content-extractor";
 import { render } from "./ui/qa";
 
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+
 let ua = navigator.userAgent;
 let browserName = ua.indexOf("Chrome") > -1 ? "Chrome" : "Firefox";
 let CORE = browserName === "Chrome" ? chrome : browser;
 let storage = chrome.storage;
 
+async function markdownToHtml(markdown) {
+  const file = await unified()
+    .use(remarkParse) // Parse Markdown to syntax tree
+    .use(remarkRehype) // Convert Markdown syntax tree to HTML syntax tree
+    .use(rehypeStringify) // Convert HTML syntax tree to HTML string
+    .process(markdown); // Process the input markdown
+
+  return String(file); // Return the processed HTML as a string
+}
+
 async function run() {
   // rendering container
-  const innerContainerBody = render(CORE);
+  const innerContainerBody = render(CORE, storage);
 
   // parsing content of original page
   let content;
@@ -22,11 +37,9 @@ async function run() {
   // handling message
   const port = CORE.runtime.connect();
 
-  port.onMessage.addListener(function (msg) {
+  port.onMessage.addListener(async function (msg) {
     if (msg.answer) {
-      innerContainerBody.innerHTML = msg.answer;
-    } else {
-      // loading
+      innerContainerBody.innerHTML = await markdownToHtml(msg.answer);
     }
   });
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,26 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./lib/src/**/*.js'],
-  prefix: 'sumz-',
-  theme: {
-    extend: {
-      animation: {
-        text: 'text 5s ease infinite',
-      },
-      keyframes: {
-        text: {
-          '0%, 100%': {
-            'background-size': '200% 200%',
-            'background-position': 'left center',
-          },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center',
-          },
-        },
-      },
-    },
-  },
-  plugins: [],
-}
-
+  content: ["./lib/src/**/*.js"],
+  plugins: [require("@tailwindcss/typography")],
+};
